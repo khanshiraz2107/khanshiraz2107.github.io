07d79eb6c0b360ba9cf8105b5536dc348ff706e6
khanshiraz2107 <khanshiraz2107@gmail.com>
2021-08-23 22:17:36 +0530

    updated

--- Module4-Solution/script.js.js
+++ Module4-Solution/script.js.js
@@ -1,15 +1,15 @@
 
 var names=new Array();
 names[0]="Yaakov";
-names[1]="John";
+names[1]="Jonathan";
 names[2]="Jen";
 names[3]="jason";
 names[4]="paul";
 names[5]="frank";
 names[6]="larry";
-names[7]="paula";
+names[7]="potter";
 names[8]="Laura";
-names[9]="jim";
+names[9]="jimmy";
 
    for (var i = 0; i < names.length; i++) {
        if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
